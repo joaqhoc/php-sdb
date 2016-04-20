dff51518546a3b75bbad4545d398e00dd6673bec
Joaquin Centurion <soportechispita@gmail.com>
2016-04-20 19:11:50 -0300

    Login | PHP - SDB
    Login y verificación en PHP sin utilizar una base de datos.

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+   <head>
+      <title>Login PHP | PHP-SDB</title>
+      <link rel="stylesheet" type="text/css" href="css/estilos_php.css">
+   </head>
+   <body>
+      <div id="ingreso">
+         <form action= "usuario.php" method="GET">
+            <label>Usuario: </label>
+            <input type="text" name="user"/>
+            <label>Contraseña: </label>
+            <input type="password" name="password"/>
+            <input type="submit" value="Enviar"/>
+         </form>
+      </div>
+   </body>
+</html>
---
+++ usuario.php
@@ -0,0 +1,56 @@
+<html>
+   <head>
+      <title>Usuario | PHP - SDB</title>
+   </head>
+   <body align="center">
+<?php
+	/* Variable usu = user - variable con = password */
+   $usu = $_GET['user'];
+   $con = $_GET['password'];
+   
+   /* Usuario 1*/
+   $usu1 = "us3r";
+   $con1 = "p4ss";
+   /* Usuario 2*/
+   $usu2 = "sk4ry";
+   $con2 = "s3c";
+   /* Usuario 3*/
+   $usu3 = "phpsdb";
+   $con3 = "sdb";
+   	
+	/* Variable Si la contraseña o usuario son incorrectos */
+   $error_notif = "¡Contraseña o usuario incorrecto!" . '<a href="'.$_SERVER['HTTP_REFERER'].'">Volver</a>';
+
+   	/* El usuario 1 es correcto */
+   switch ($usu) {
+      case $usu1:
+         if ($con == $con1) {
+            echo "Bienvenido " . $usu;
+			echo "<br>Ir al "  . '<a href="index.php">Inicio</a>';
+         } else {
+            echo $error_notif; /* El usuario 1 es incorrecto */
+         }
+      break;
+	  
+	/* El usuario 2 es correcto */
+      case $usu2:
+         if ($con == $con2) {
+            echo "Bienvenido " . $usu; 
+         } else {
+            echo $error_notif; /* El usuario 2 es incorrecto */
+         }
+      break;
+	/* El usuario 3 es correcto */
+      case $usu3:
+         if ($con == $con3) {
+            echo "Bienvenido " . $usu;
+         } else {
+            echo $error_notif; /* El usuario 3 es incorrecto */
+         }
+      break;
+      default:
+         echo $error_notif;
+   }
+?>
+   </body>
+</html>
